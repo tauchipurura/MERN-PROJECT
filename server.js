fa8b21141453714db43290a200b48c1969c4de16
tau chipurura <tauraichipurura@gmail.com>
2023-05-27 01:20:19 +0200

    Listening to ports

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+
+
+
+const logEvents = require('./logEvents')
+const EventEmitter = require('events')
+class Emitter extends EventEmitter{};
+// initialize object
+const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req,res)=> {
+    console.log(req.url, req.method)
+
+    
+})
+
+server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+
+// myEmitter.on('log', (msg)=> logEvents(msg));
+
+//     myEmitter.emit('log','log event emitted')
